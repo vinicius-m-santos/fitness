b03e1a5ff3a3a5afcb7e7a9693ae2038f6ac3b69
Vinicuis Santos <vinima100@gmail.com>
2026-02-05 22:13:42 -0300

    feat(FIT-61): Adding sentry backend and front

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,10 +8,16 @@ import "./i18n";
 import { AuthProvider } from "./providers/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import * as Sentry from "@sentry/react";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 const queryClient = new QueryClient();
+
+Sentry.init({
+    dsn: "https://5d922fe9f235a78a033a1a756c9914ab@o4510824276099072.ingest.us.sentry.io/4510824278720512",
+    sendDefaultPii: true
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
